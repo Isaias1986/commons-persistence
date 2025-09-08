6fbeff19757a45880aed7e55f38ea41816f0bd55
isita <isita@banregio.com>
2025-09-08 10:45:20 -0600

    primer commit de la rama principal

---
+++ src/main/java/com/iep/commons/persistence/CommonsIepPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.iep.commons.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsIepPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonsIepPersistenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iep/commons/persistence/CommonsIepPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iep.commons.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsIepPersistenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
